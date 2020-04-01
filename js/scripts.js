70d538fccfeee7dbd0df7acefadfd9a953319def
me <me@me.me.com>
2020-04-01 14:34:29 -0700

    add 3 more forms for sub/mult/divide

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,49 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+$(document).ready(function(){
 
-alert(divide(number1, number2));
+  $("form#add").submit(function(e){
+    e.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#subtract").submit(function(e){
+    e.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#multiply").submit(function(e){
+    e.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#divide").submit(function(e){
+    e.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
+  
+});
 
 
 
 
+
+
+1
